ed4ff4a3437cb44922aea7d4ffd44b583f00ca97
keybladeSkai <asuleodu@gmail.com>
2024-08-15 16:23:19 +0100

    new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Header from "./components/header/Header"
-import Body from "./components/body/Body"
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
 
 const App = () => {
   return (
     <div className="h-[100vh] px-3 pt-6 flex flex-col gap-1 overflow-hidden bg-[--bg-color]">
-      <Header/>
-      <Body/>
+      <Header />
+      <div className="overflow-scroll h-screen">
+        <Body />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
