107b6531310ac6e637286c31e9977a45dda38583
keybladeSkai <asuleodu@gmail.com>
2024-08-15 16:57:49 +0100

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ const App = () => {
   return (
     <div className="h-[100vh] px-3 pt-6 flex flex-col gap-1 overflow-hidden bg-[--bg-color]">
       <Header />
-      <div className="overflow-scroll">
-        <Body />
-      </div>
+      <Body />
     </div>
   );
 };
